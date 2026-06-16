0ff26004ed169f596120b3d285d68e6dcec0ed53
Shuvo <shuvot01794@gmail.com>
2026-06-16 23:06:00 +0600

    chore: configure UI dependencies and styling tools

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,9 +3,16 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!-- font-awesome CDN -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <!-- Google Font -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
         <x-inertia::head />
     </head>
-    <body>
+
+    <body class="font-montserrat">
         <x-inertia::app />
         @routes
     </body>
